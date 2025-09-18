4d46773f97cb4f29678e6d0e301f786c6b398d3e
Rishin Dutta <rishin.dutta2004@gmail.com>
2025-09-19 02:19:55 +0530

    Added a new Security Config File for managing the security and removed the user and password from application.properties file

---
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain setSecurityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf((csrf) -> csrf.disable())
+            .authorizeHttpRequests((authorize) -> authorize.anyRequest()
+                    .authenticated()).httpBasic(Customizer.withDefaults());
+    return http.build();
+   }
+
+   @Bean
+    public UserDetailsService userDetailsService() {
+       UserDetails userDetails= User.builder().
+               username("rishin").password("rishin_007").roles("USER").build();
+
+       UserDetails adminDetails=User.builder().username("admin")
+               .password("admin_007").roles("ADMIN").build();
+       return new InMemoryUserDetailsManager(userDetails, adminDetails);
+   }
+}
