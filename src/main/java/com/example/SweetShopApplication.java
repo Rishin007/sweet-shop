b836f42e460493da174acd19174017f4aeed3d82
Rishin Dutta <rishin.dutta2004@gmail.com>
2025-09-18 19:52:53 +0530

    Demo Sweets that were created from CommandLineRunner have been removed and now we can directly create sweet from PostMapping using Postman API tool.

--- src/main/java/com/example/SweetShopApplication.java
+++ src/main/java/com/example/SweetShopApplication.java
@@ -1,37 +1,18 @@
 package com.example;
 
-import com.example.entity.Sweets;
 import com.example.repository.SweetRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
+
 
 @AllArgsConstructor
 @SpringBootApplication
-public class SweetShopApplication implements CommandLineRunner {
+public class SweetShopApplication  {
 private SweetRepository sweetRepo;
     public static void main(String[] args) {
         SpringApplication.run(SweetShopApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-
-        sweetRepo.saveAll(
-                List.of(
-                        new Sweets("Rasogolla","Misti",58.66,8),
-                        new Sweets("Misti Doi","Curd",98.66,28),
-                        new Sweets("Raaj Bhog","Misti",168.44,77),
-                        new Sweets("Kaaju Katli","Dry Sweets",58.66,45),
-                        new Sweets("Soan Papdi","Dry Sweets",58.66,77),
-                        new Sweets("Gulab Jamun","Misti",180.24,58),
-                        new Sweets("Baarfi","Dry Sweets",24.79,37)
-
-                )
-        );
-        System.out.println("Sweets saved successfully");
-    }
 }
